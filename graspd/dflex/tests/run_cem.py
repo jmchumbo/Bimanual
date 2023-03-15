06557d7e531a81a3201f1f026b6f2ec0c2c69b30
jmchumbo <jmchumbo@gmail.com>
2023-03-15 09:44:05 +0000

    check torch working

--- graspd/dflex/tests/run_cem.py
+++ graspd/dflex/tests/run_cem.py
@@ -17,20 +17,25 @@ print('\n'.join(sys.path)) # does this show the files and folders you need?
 import hydra
 from hydra.utils import to_absolute_path
 
+
+
+#A few tests just to check how to append the path to dflex
+"""
 #print("\n Append: \n")
 #sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'src'))
 
-
-
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-print("\n Append dflex: \n")
-sys.path.append('/home/j-chumbinho/Bimanual/graspd/')
 
 #print("Where am I? - " + os.getcwd())
 
 #print("2 - What do we need? \n")
 #print('\n'.join(sys.path)) # does this show the files and folders you need?
+"""
+
+#print("\n Append dflex: \n")
+sys.path.append('/home/j-chumbinho/Bimanual/graspd/')
+
+print(torch.cuda.is_available())
 
 exit()
 
